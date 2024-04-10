02ecfd95436222bed3e826745ca9933751908cd7
paulaarroyo <p.arroyosalvo@gmail.com>
2024-04-09 22:37:23 -0700

    Add Color Scheme

--- app/src/main/java/com/namkuzo/feriaspot/MainActivity.kt
+++ app/src/main/java/com/namkuzo/feriaspot/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +19,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FeriaSpotTheme {
                 // A surface container using the 'background' color from the theme
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Demo")
+                }
             }
         }
     }
